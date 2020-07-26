9ae4da1625ec7fadc9c8d67fcc3f27481539e29b
Richard <Richard>
2020-07-26 13:47:08 -0700

    add cairo

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-extern crate gtk;
-use crate::gtk::prelude::*;
+use gtk::prelude::*;
 use gtk::*;
+use std::f64::consts::PI;
 
 fn main() {
     if gtk::init().is_err() {
@@ -14,7 +14,8 @@ fn main() {
 
     let window: gtk::Window = builder.get_object("window1").unwrap();
     let button: gtk::Button = builder.get_object("button1").unwrap();
-
+    let canvas: gtk::DrawingArea = builder.get_object("drawable1").unwrap();
+    
     // set global style
     let screen = window.get_screen().unwrap();
     let style = include_str!("style.css");
@@ -26,6 +27,22 @@ fn main() {
     button.connect_clicked(move |_| {
         println!("exiting app!");
         std::process::exit(0);
+    });
+
+    // handle draw and use cairo context
+    canvas.connect_draw(|_, ctx| {
+        ctx.scale(100f64, 100f64);
+        ctx.set_source_rgba(1.0, 0.2, 0.2, 0.6);
+        ctx.arc(0.4, 0.53, 0.2, 0.0, PI * 2.);
+        ctx.fill();
+        ctx.set_source_rgba(1.0, 0.2, 0.92, 0.6);
+        ctx.arc(0.47, 0.65, 0.2, 0.0, PI * 2.);
+        ctx.fill();
+        ctx.set_source_rgba(0.0, 0.2, 0.92, 0.6);
+        ctx.arc(0.5, 0.53, 0.2, 0.0, PI * 2.);
+        ctx.fill();
+
+        Inhibit(false)
     });
 
 
