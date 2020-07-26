b4c87c60a2c98e3d4382781d247e31552e3037b8
Richard <Richard>
2020-07-26 09:05:36 -0700

    adding more comments

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 extern crate gtk;
-use crate::gtk::prelude::BuilderExtManual;
+use crate::gtk::prelude::*;
 use gtk::*;
 
 fn main() {
@@ -7,6 +7,7 @@ fn main() {
         println!("Failed to initialize GTK.");
         return;
     }
+
     let glade_src = include_str!("app.glade");
 
     let builder = gtk::Builder::from_string(glade_src);
@@ -14,9 +15,23 @@ fn main() {
     let window: gtk::Window = builder.get_object("window1").unwrap();
     let button: gtk::Button = builder.get_object("button1").unwrap();
 
-    button.connect_clicked(move |_| println!("foo"));
+    // set global style
+    let screen = window.get_screen().unwrap();
+    let style = include_str!("style.css");
+    let provider = CssProvider::new();
+    provider.load_from_data(style.as_bytes()).unwrap();
+    gtk::StyleContext::add_provider_for_screen(&screen, &provider, STYLE_PROVIDER_PRIORITY_APPLICATION);
 
+    // exit app on click
+    button.connect_clicked(move |_| {
+        println!("exiting app!");
+        std::process::exit(0);
+    });
+
+
+    window.set_decorated(false);
     window.show_all();
+    window.maximize();
 
     gtk::main();
 }
